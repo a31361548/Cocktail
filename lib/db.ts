031df9904618e0b95346beee98db4eb4752a2494
Daniel <daniel@chentchitech.com>
2025-07-24 17:13:11 +0800

    🎨 Fixed eslintError

--- lib/db.ts
+++ lib/db.ts
@@ -13,10 +13,11 @@ interface MongooseCache {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 let cached: MongooseCache = (global as any).mongoose;
 
 if (!cached) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
